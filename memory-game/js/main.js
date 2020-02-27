ac5b5c7b086a95ac1ab255afd9774587a204186d
kwansing14 <59598889+kwansing14@users.noreply.github.com>
2020-02-28 00:29:42 +0800

    commit game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const cards=["queen", "queen", "king", "King"];
+const cardsInPlay=[];
+let cardOne;
+
+cardOne = cards[1];
+cardsInPlay.push(cardOne);
+
+console.log(cardsInPlay[0]);
+console.log("User Flipped queen");
+
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped king");
+
+if (cardsInPlay.length === 2) {
+	console.log("there's 2 cards");
+}
+
+if (cardsInPlay[0] == cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
