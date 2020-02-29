58caf98047c98862992852aa61ee10ed328406d3
kwansing14 <59598889+kwansing14@users.noreply.github.com>
2020-02-29 17:24:09 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,25 +2,31 @@ console.log("Up and running!");
 
 const cards=["queen", "queen", "king", "King"];
 const cardsInPlay=[];
-let cardOne;
 
-cardOne = cards[1];
-cardsInPlay.push(cardOne);
+function checkForMatch () {
 
-console.log(cardsInPlay[0]);
-console.log("User Flipped queen");
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		alert("You found a match!");
+	}	else {
+		alert("Sorry, try again");
+	}
 
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped king");
-
-if (cardsInPlay.length === 2) {
-	console.log("there's 2 cards");
 }
 
-if (cardsInPlay[0] == cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again");
+function flipCard(cardID, cardID2) {
+
+	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID2]);
+
+	console.log("User flipped " + cards[cardID]);
+	console.log("User flipped " + cards[cardID2]);
+
+	if (cardsInPlay.length === 2) {
+		console.log("there's 2 cards");
+	}
+
+	checkForMatch();
+
 }
+
+flipCard(1, 2);
